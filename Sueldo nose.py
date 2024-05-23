01fa704e3b97883aa1d6eaa73e3a83b41b5f322b
sebagunckelcom <sebastianalejandro.gunckel@alumnos.ulagos.cl>
2024-05-22 22:42:55 -0400

    Delete Sueldo nose.py

--- Sueldo nose.py
+++
@@ -1,8 +0,0 @@
-sueldoh = int(input("ingrese el sueldo por hora del trabajador: "))
-dias = int(input("ingrese la cantidad de dias trabajados: "))
-
-sueldod = sueldoh * 8
-print("el sueldo diario es de: ", sueldod)
-
-sueldot = sueldod * dias
-print("el sueldo mensual es de: ", sueldot)
