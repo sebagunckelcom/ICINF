6a6b6af5deecd37399c96d0a6531d15db0506dcc
sebagunckelcom <sebastianalejandro.gunckel@alumnos.ulagos.cl>
2024-05-22 22:43:16 -0400

    Delete errorrrr.py

--- errorrrr.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("ingrese el primer valor: "))
-num2 = int(input("ingrese el segundo valor: "))
-suma = num1 + num2
-producto = num1 * num2
-print("La suma de ambos numeros es")
-print(suma)
-print("La multiplicacion de ambos numeros es")
-print(producto)
